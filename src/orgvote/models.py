ab1d6bd1ba203957232a8dd29dee54d88d1e8a9d
dahle <isaacbd.personal@gmail.com>
2023-04-14 16:06:27 -0700

    Adding models

--- src/orgvote/models.py
+++ src/orgvote/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Organization(models.Model):
+    name = models.CharField()
+
+class Topic(models.Model):
+    name = models.CharField()
+
+class Survey(models.Model):
+    name = models.CharField() 
+
+class (models.Model):
+    pass
