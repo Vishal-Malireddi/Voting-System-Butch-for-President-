06bc8562135fd73a428d923e799d7303e50794b2
dahle <isaacbd.personal@gmail.com>
2023-04-14 15:03:05 -0700

    Finished removing the old root folder

--- project/voting_system/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-# define the urls that will be used under the voting_system/ domain
-urlpatterns = [
-    path("", views.index, name="index"),
-
-
-]
--- project/voting_system/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-# a basic hello world text view
-def index(request):
-    return HttpResponse("Hello world. You are at the voting_sytem index.")
