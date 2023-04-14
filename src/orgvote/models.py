5e09e44295cacc5f8216a64449accc2e9033c6e4
dahle <isaacbd.personal@gmail.com>
2023-04-14 16:13:29 -0700

    Added more models

--- src/orgvote/models.py
+++ src/orgvote/models.py
@@ -11,6 +11,13 @@ class Topic(models.Model):
 
 class Survey(models.Model):
     name = models.CharField() 
+    topic = models.ForeignKey(Organization, on_delete=models.CASCADE)
 
-class (models.Model):
-    pass
+class Question(models.Model):
+    question_text = models.CharField() 
+    survey = models.ForeignKey(Organization, on_delete=models.CASCADE)
+
+class Choice(models.Model):
+    choice_text= models.CharField() 
+    question = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    votes = models.IntegerField(default=0)
