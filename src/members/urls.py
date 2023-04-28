ad26465d571617ab0f3e6a36734750cd88883a6b
dahle <isaacbd.personal@gmail.com>
2023-04-28 09:38:31 -0700

    Added / to the end of the paths in members/urls.py

--- src/members/urls.py
+++ src/members/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name='members'
 urlpatterns = [
-    path('login_user', views.login_user, name="login"),
-    path('register_user', views.register_user, name="register_user"),
+    path('login_user/', views.login_user, name="login"),
+    path('register_user/', views.register_user, name="register_user"),
 ]
